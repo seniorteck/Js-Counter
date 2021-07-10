4b3abd940da6b0f825e02b06336320e822885af2
code_kareem <seniorteck4real@gmail.com>
2021-07-09 20:33:23 -0400

    increment button fix

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
-const decrement = document.querySelector("#decrement");
+// select the button ;
+// store the select buttons in a variable;
+// select the output & store it in a variable;
+// add an event listener of click to the increment variable;
+// add an event listener of click to the decrement variable;
+
 
 const increment = document.querySelector("#increment");
+
+const decrement = document.querySelector("#decrement");
+
+let output = document.querySelector("#output")
+
+
+increment.addEventListener('click', ()=>{
+
+    // let i = 0
+    // while(i<=50){
+    //     console.log(i);
+    //     i++
+
+        
+    // }
+
+    let result = output.textContent++;
+
+    console.log(result)
+    
+});
+
+decrement.addEventListener('click', ()=>{
+    console.log("you clicked me to decrement");
+})
+
+
+let incrementAdd = ()=>{
+
+}
+
+
+
+
