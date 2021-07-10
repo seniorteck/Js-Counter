87a0bb65de2df920cd0d4f42a08b5bd8e4b53aff
code_kareem <seniorteck4real@gmail.com>
2021-07-10 17:15:36 -0400

    max & min Count set

--- js/app.js
+++ js/app.js
@@ -15,22 +15,25 @@ let output = document.querySelector("#output")
 increment.addEventListener('click', ()=>{
     let result = output.textContent++;
 
-    if (result===50){
-        result.reset()
-    }else{
-        
+    if (result >= 50){
+        result=output.textContent = "Max reach";
+
+        result.style.fontSize = "12px";
     }
     
 });
 
 decrement.addEventListener('click', ()=>{
     let result = output.textContent--;
+
+    if (result <= 0){
+        result=output.textContent = "Min reach";
+        
+        result.style.fontSize = "12px";
+    }
 })
 
 
-let incrementAdd = ()=>{
-
-}
 
 
 
